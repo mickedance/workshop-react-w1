e67e2709e5841c5bf7a7da1d5b54ff9e78359871
mickedance <mickedance@hotmail.com>
2022-09-19 09:50:16 +0200

    first commit

---
+++ workshop1/src/index.js
@@ -0,0 +1,3 @@
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+ReactDOM.render(<div className='btn btn-primary'>Hello React!</div>, document.getElementById('root'));
